5cdc400e1e52984f77e805762cc8b6120f0ebbbc
Sebahattin Tonbul <sebahattint@gmail.com>
2013-10-30 14:22:19 +0200

    Login ve Master Page
    Login form yapıldı. Master page oluşturuldu. Master page kullanılacak
    olan viewları oluşturuken, use master page seçeneğini işaretle, shared
    daki sitemaster ı seç. Ok ;)

--- SmartAccount/SmartAccount/App_Start/RouteConfig.cs
+++ SmartAccount/SmartAccount/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SmartAccount
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "User", action = "vwUser", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SmartAccount/SmartAccount/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SmartAccount.Models;
+using System.Web.Security;
+namespace SmartAccount.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            //SmartAccountEntities entity = new SmartAccountEntities();
+            //User kul = new User() { UserId = Guid.NewGuid(), UserLoginName = "Sebahattin TONBUL", UserName = "stonbul" };
+            //entity.Users.AddObject(kul);
+            //entity.SaveChanges();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel lmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (lmodel.UserName == "a" && lmodel.Password == "1")
+                {
+                    FormsAuthentication.SetAuthCookie(lmodel.UserName, false);
+                    return RedirectToAction("index", "home"); //sayfa adı - klasör adı
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login failed.");
+                }
+
+            }
+            return View();
+        }
+
+    }
+}
